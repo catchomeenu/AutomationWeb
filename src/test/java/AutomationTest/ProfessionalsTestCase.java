c6de8e31ad0352b2a45d0f152f6015814b73e58a
pri0911 <priyajadhawar1994@gmail.com>
2024-08-01 20:17:13 -0400

    Changes Priyanka J

---
+++ src/test/java/AutomationTest/ProfessionalsTestCase.java
@@ -0,0 +1,37 @@
+package AutomationTest;
+
+import Automation.pageobjects.Professionals;
+
+import Automation.pageobjects.Professionals;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+
+public class ProfessionalsTestCase extends Professionals {
+
+    @BeforeSuite
+    public static void lauchApplication() {
+        launchBrowser("Chrome");
+        launchApp("https://devnj.infyni.com/");
+        implicitWait(30);
+    }
+    @Test(priority = 1)
+    public  void Courses()
+    {
+        Home();
+        System.out.println("Navigated to 'Home' link");
+        ProfessionalCourse();
+        System.out.println("Navigated to 'Professional' link");
+        boolean actualresult = ProfessionalPage();
+        boolean expectedresult = true;
+        SoftAssert sa = new SoftAssert();
+        sa.assertEquals(actualresult, expectedresult);// using soft assert
+        sa.assertAll();
+        System.out.println("Professional text presence verified");
+        Home();
+        System.out.println("Navigated back to homepage from Professional page");
+
+    }
+}
--- src/test/java/AutomationTest/StudentDashboardPageTestCases.java
+++ src/test/java/AutomationTest/StudentDashboardPageTestCases.java
@@ -49,4 +49,5 @@ public class StudentDashboardPageTestCases extends StudentDashBoardPage
 
             }
 
+
 }
